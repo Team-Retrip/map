53f831e3cda51cc710eb8c008f7ea4716a705a79
jisubaek <88620009+jisubaek@users.noreply.github.com>
2026-01-26 21:58:52 +0900

    Replace MySQL connector with MariaDB client

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.mysql:mysql-connector-j:8.0.33")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.3.3")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.batch:spring-batch-test")
